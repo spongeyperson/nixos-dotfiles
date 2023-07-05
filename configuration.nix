75098bb43f866a25ed838dfc9becbffe0188bc74
spongeyperson <commits@spongey.xyz>
2023-07-05 00:56:13 -0700

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,218 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+
+  networking.hostName = "Spongey-PC"; # Define your hostname.
+  # Pick only one of the below networking options.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  # Set Plasma Default Session to Wayland
+  services.xserver.displayManager.defaultSession = "plasmawayland";
+
+
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # Pulseaudio (Gross)
+  # hardware.pulseaudio.enable = true;
+
+  # Pipewire
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tyler = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "disk" "libvirtd" "docker" "audio" "video" "input" "systemd-journal" "networkmanager" "network" "davfs2" ];
+    packages = with pkgs; [
+      # Userspace,GUI
+      steam
+      qpwgraph
+      # Userspace,GUI,noflatpak
+      brave
+      firefox
+      chromium
+      mailspring
+      webcord
+      spotify
+      # Userspace,CLI
+      flatpak
+      android-tools
+      scrcpy
+      neofetch
+      tree
+      jdk8
+      jdk17
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # editors
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    # tools, cli
+    wget
+    curl
+    unzip
+    htop
+    btop
+    tmux
+    git
+    man
+    lshw
+    ffmpeg
+    ncurses5
+    coreutils
+    binutils
+    pciutils
+    dmidecode
+    # make tools, cli
+    autoconf
+    gcc
+    gnumake
+    llvm
+    libclang
+    clang
+    cmake
+    # tools, gui
+    gparted
+    kdiff3
+    vlc
+    etcher
+    # art
+    krita
+    # virtualization, qemu
+    spice
+    docker-compose
+    virt-manager
+    gnome3.dconf # needed for saving settings in virt-manager
+    libguestfs # needed to virt-sparsify qcow2 files
+    libvirt
+    # fsmount,webdav
+    davfs2
+    autofs5
+    fuse
+    sshfs
+    cadaver
+  ];
+  # Fonts
+  fonts.fonts = with pkgs; [
+    nerdfonts
+    powerline-fonts
+    cascadia-code
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+  # FIX "GTK themes are not applied in Wayland applications"
+  # https://nixos.wiki/wiki/KDE#GTK_themes_are_not_applied_in_Wayland_applications
+  programs.dconf.enable = true;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.flatpak.enable = true;
+
+  #Virt-Manager Services
+  services.qemuGuest.enable = true;
+
+  virtualization.libvirtd = {
+    enable = true;
+    qemuOvmf = true;
+    qemuRunAsRoot = true;
+    onBoot = "ignore";
+    onShutdown = "shutdown";
+  };
+
+  virtualization.docker.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
+
