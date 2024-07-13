50ddd75523e95be1634987523a039e7d1de7b853
spongeyperson <commits@spongey.xyz>
2024-07-12 22:56:49 -0700

    Sleep commit
    I need to sleep, but i want to make sure i have a backup, lol

--- etc/nixos/system/systemPackages.nix
+++ etc/nixos/system/systemPackages.nix
@@ -23,7 +23,7 @@
     localBinInPath = true;
     systemPackages = with pkgs; [
       # Packages with Insecure Package Requirements
-      #etcher
+      etcher
 
       # Text Editors - CLI
       vim
@@ -73,6 +73,7 @@
       viddy
       fzf
       duf
+      pv
       atuin
       du-dust
       bat
